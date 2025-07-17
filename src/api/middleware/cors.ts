cf7c9011e49b07fe66aeec6d2af059f4c6c8c7ef
Delwo Athauda <delwo2003@gmail.com>
2025-07-17 10:51:52 +0530

    Update CORS middleware to include additional allowed origins for local development

--- src/api/middleware/cors.ts
+++ src/api/middleware/cors.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 const whitelist = new Set([
   "http://localhost:5173",
   "http://localhost:5174",
+  "http://localhost:3000",
+  "http://192.168.1.33:3000/",
   "https://office-it-front-end.vercel.app/",
   "https://officeit.lk",
 ]);
